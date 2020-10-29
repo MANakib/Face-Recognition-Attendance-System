3c0b5d74b2a9387b3c6a2c8fb6829670c7537743
M A Nakib <nakib_juris@outlook.com>
2020-10-29 15:22:12 +0600

    Update firstpage.py

--- attendance/firstpage.py
+++ attendance/firstpage.py
@@ -1,6 +1,6 @@
 #import module from tkinter for UI
 from tkinter import *
-from playsound import playsound
+#from playsound import playsound
 import os
 from datetime import datetime;
 #creating instance of TK
